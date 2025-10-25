8c1f6dae430e26a90565d2d8dba6cef22fb19154
Bartłomiej K <bartekk20@users.noreply.github.com.>
2025-10-25 18:10:02 +0200

    Zadanie 9 Python lab.2

---
+++ zadanie9.py
@@ -0,0 +1,7 @@
+# Zad 9. dodatkowe
+# Napisz skrypt zmieniający wszystkie duże litery małe i na odwrót.
+# Podpowiedz: skorzytaj z metody swapcase().
+
+ciag_znakow = str(input("Podaj ciąg znaków WIELKIMI lub MAŁYMI literami: "))
+
+print(ciag_znakow.swapcase())
