604c78e392b915f853d53cf87715be3cf9383078
Bartłomiej K <bartekk20@users.noreply.github.com.>
2025-10-25 19:40:57 +0200

    Zadanie 8 Python lab.2

---
+++ zadanie8.py
@@ -0,0 +1,14 @@
+# Zad 8.
+# Stwórz program, który wykorzystując operator wycinania z podanego ciąg znaków (zmienna text)
+# wyodrębni:
+# pierwsze trzy znaki
+# ostatnie dwa znaki
+# Wynik wydrukuj do konsoli.
+
+text = 'Studiuje-Informatykę'
+
+pierwsze_trzy_znaki = text[0:3]
+print(pierwsze_trzy_znaki)
+
+ostatnie_dwa_znaki = text[-2:]
+print(ostatnie_dwa_znaki)
