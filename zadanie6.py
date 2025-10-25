f3937f739527407911c2aaba65c6369b92d4f193
Bartłomiej K <bartekk20@users.noreply.github.com.>
2025-10-25 17:48:43 +0200

    Zadanie 6 Python lab. 2

---
+++ zadanie6.py
@@ -0,0 +1,13 @@
+# Zad. 6 
+# Napisz skrypt w Pythonie, który sprawdza, czy litera wprowadzona przez użytkownika jest duża czy mała
+
+print("Program sprawdzjący czy podana litera przez użytkownika jest wielka lub mała")
+
+litera = str("Podaj literę: ")
+
+if litera.isupper():
+    print("Litera jest wielka")
+elif litera.islower():
+    print("Litera jest mała")
+else:
+    print("Podaj literę!")
