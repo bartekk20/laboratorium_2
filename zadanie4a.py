c006c4207949856f73b8f3fdb56aacf91197953d
Bartłomiej Kwiatkowski <bartekk20@users.noreply.github.com>
2025-10-25 11:07:01 +0200

    Zadania dodatkowe Python lab.2

---
+++ zadanie4a.py
@@ -0,0 +1,32 @@
+# Zadanie 4 a
+# Jesteś menagerem drużyny piłkarskiej i chcesz obliczyć łączny wynik drużyny na podstawie liczby
+# strzelonych przez nią bramek i ewentualnie zdobytych dodatkowych punktów. Napisz program,
+# który dokona stosownych kalkulacji po wprowadzeniu liczby goli zdobytych przez drużynę.
+# Utworzone są dwie zmienne gol i bonus, gdzie gol to liczba całkowita reprezentująca liczbę
+# bramek zdobytych przez drużynę, a bonus to liczba całkowita reprezentująca wszelkie możliwe
+# punkty bonusowe dla drużyny.
+# Następnie użyj instrukcji warunkowej do obliczenia całkowitego wyniku zespołu zgodnie z
+# następującymi zasadami:
+# - każda zdobyta bramka to 10 punktów,
+# - jeśli drużyna zdobędzie więcej niż 5 bramek, zdobywa dodatkowe 5 punktów bonusowych,
+# - jeśli drużyna zdobędzie więcej niż 10 bramek, zdobywa dodatkowe 10 punktów bonusowych
+# a) Po zdobyciu 5 goli drużyna otrzymuje 5 punktów bonusowych. Jeśli drużyna zdobędzie więcej
+# niż 10 goli, to otrzyma za nie 10 punktów bonusowych dodatkowo
+# Oblicz całkowity wynik drużyny, dodając punkty zdobyte ze zdobytych bramek i wszelkie
+# stosowne punkty bonusowe. Wynik wydrukuj do konsoli.
+# b) Punkty bonusowe po przekroczeniu 5 i 10 punktów są sumowane, tzn. po przekroczeniu więcej
+# niż 10 bramek drużyna zdobywa obydwa bonusy.
+# Oblicz całkowity wynik drużyny, dodając punkty zdobyte ze zdobytych bramek i wszelkie
+# stosowne punkty bonusowe. Wynik wydrukuj do konsoli.
+
+gol = int(input("Podaj liczbę goli: "))
+bonus = 0
+
+bramka = 10
+
+if gol > 5:
+    bonus = 5
+if gol > 10:
+    bonus = 10
+
+print("Suma punktów: ", (gol * bramka) + bonus)
