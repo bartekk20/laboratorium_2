0e16db22847f5e2b3ec05d3d18f04887be382d0f
Bartłomiej K <bartekk20@users.noreply.github.com.>
2025-10-31 15:33:45 +0100

    .

--- zadanie2.py
+++ zadanie2.py
@@ -2,14 +2,15 @@
 # Napisz program porządkowania trzech liczb x, y i z. Od najmniejszej do największej, bez użycia
 # wbudowanych funkcji
 
-x = input("Podaj 1 liczbę: ")
-y = input("Podaj 2 liczbę: ")
-z = input("Podaj 3 liczbę: ")
+x = int(input("Podaj 1 liczbę: "))
+y = int(input("Podaj 2 liczbę: "))
+z = int(input("Podaj 3 liczbę: "))
 
 if x > y:
     x, y = y, x
 if x > z:
-    x, z = z,x
+    x, z = z, x
 if y > z:
-    y, z = z,y
-print("Posortowane liczby: ", x,y,z)
+    y, z = z, y
+
+print("Liczby od najmniejszej do największej:", x, y, z)
